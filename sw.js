66c4c7d208f48cc2966e3cb62328cd3cb20474c3
marchinomelis1985-del <marchinomelis1985@gmail.com>
2026-09-10 22:55:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'softair-hq-phoenix-v4';
+const CACHE_NAME = 'softair-hq-phoenix-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
